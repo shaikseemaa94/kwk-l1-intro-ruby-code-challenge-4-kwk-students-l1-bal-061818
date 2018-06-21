08242db086e6c994b48ffed8c994d554e63cbfb6
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-21 17:48:05 +0000

    Done.

--- spec/least_coins_spec.rb
+++ spec/least_coins_spec.rb
@@ -1,11 +1,18 @@
 require_relative './spec_helper'
 
+
+def least_coins(cents)
+puts "Type in a number for least coins"
+input = gets.chomp
+puts expect(least_coins(29)).to eq({:quarters => 1, :dimes => 0, :nickels => 0, :pennies => 4})
+
+
 describe 'LEAST COINS' do
-  
+
   describe '#least_coins' do
     it "returns the least amount of coins" do
-    
-    expect(least_coins(29)).to eq({:quarters => 1, :dimes => 0, :nickels => 0, :pennies => 4}) 
+
+    expect(least_coins(29)).to eq({:quarters => 1, :dimes => 0, :nickels => 0, :pennies => 4})
 
     expect(least_coins(99)).to eq({:quarters => 3, :dimes => 2, :nickels => 0, :pennies => 4})
     end
