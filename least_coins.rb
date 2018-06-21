6db75657524bc7d62b38eb9c154055ea05f2d832
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-21 17:47:27 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,19 @@
-#write out your code here
-
 def least_coins(cents)
 
-#Code your answer here!
+coins = {
+  :"quarters" => 25,
+  :"dimes" => 10,
+  :"nickels" => 5,
+  :"cents" => 1
+}
+finalCoins = {}
 
+coins.each do |koin, amount|
+   num= cents/amount
+   cents -= num*amount
+   finalCoins[koin]= num
+ end
+
+puts finalCoins
 end
+least_coins(20)
